0d49a8bb1f638281acbe8c6c087751872df70936
Jimmy Ungerman <jimmy.ungerman@gmail.com>
2023-07-13 12:13:48 -0600

    one node group

--- infra/eks.tf
+++ infra/eks.tf
@@ -25,15 +25,5 @@ module "eks" {
       max_size     = 3
       desired_size = 1
     }
-    two = {
-      name = "node-group-2"
-
-      instance_types = ["t2.small"]
-      capacity_type  = "SPOT"
-
-      min_size     = 1
-      max_size     = 3
-      desired_size = 1
-    }
   }
 }
