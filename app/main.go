bbd2f0165ac7587da2b1f87444180822ee7febfe
Jimmy Ungerman <jimmy.ungerman@gmail.com>
2023-07-13 11:57:41 -0600

    lets go again

--- app/main.go
+++ app/main.go
@@ -20,6 +20,7 @@ func golangAPI(w http.ResponseWriter, r *http.Request){
 	body, err := json.Marshal(response)
 
 	// Error checking brought up by linting job
+	// Add a comment to deply
 	if err != nil {
 		panic(err)
 	}
